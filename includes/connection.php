a72fada6ee6acaf0f276c97a622a624fa20806c0
philbertyu <51773244+philbertyu@users.noreply.github.com>
2020-04-18 09:10:53 -0700

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-  $con = mysqli_connect('localhost','root','','scout');
+  $con = mysqli_connect('us-cdbr-iron-east-01.cleardb.net','b8a2927a50099e','8036e8df','scout');
 
   if(!$con)
   {
