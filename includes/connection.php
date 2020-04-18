039df78537507d9c5c55357c210bff15fa505f26
philbertyu <51773244+philbertyu@users.noreply.github.com>
2020-04-18 09:37:12 -0700

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-  $con = mysqli_connect('us-cdbr-iron-east-01.cleardb.net','b8a2927a50099e','8036e8df','scout');
+  $con = mysqli_connect('us-cdbr-iron-east-01.cleardb.net','b8a2927a50099e','8036e8df','heroku_c1c6c2ef5faa08f');
 
   if(!$con)
   {
