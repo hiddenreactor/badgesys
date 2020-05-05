89345953e0c4bad6ce23bc0b0e5b88bc3ab9e17b
philbertyu <51773244+philbertyu@users.noreply.github.com>
2020-05-05 16:10:04 -0700

    update login.php with index.php

--- includes/header.php
+++ includes/header.php
@@ -4,18 +4,14 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    
-  
-    
+       
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>  
+  <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>    
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
   
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  
-  
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <link rel="stylesheet" href="bootstrap/bootstrap.css">
   <link rel="stylesheet" href="css/style.css">
     
@@ -40,7 +36,7 @@
                 }
                 else
                 {
-                  echo '<a href="login.php" style="color:black; class="bg-light topnav-right">Login</a>';
+                  echo '<a href="index.php" style="color:black; class="bg-light topnav-right">Login</a>';
                 }
               ?>
             </li>
