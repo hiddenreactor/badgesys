be93160c852afc416298c8f93c4048bd49cd6bac
philbertyu <51773244+philbertyu@users.noreply.github.com>
2020-11-13 13:52:40 -0800

    new index.php
    new index.php with regular user/admin user merge login

--- index.php
+++ index.php
@@ -1,34 +1,48 @@
-<?php 
-
+<?php
 require_once('includes/header.php');
 require_once('includes/function.php');
 ?>
 
+
 <div class="container">
   <div class="row">
-    <div class="col-lg-6">
-        <div class="card bg-dark text-white mt-5">
-          <h3 class="text-center py-3"> Leader Login </h3>
+    <div class="col-lg-6 m-auto">
+      <div class="mt-5">
+      </div>
+
+      <div class="card">
+        <div class="card-title bg-dark rounded-top">
+          <h5 class="text-center text-white py-3">Kerrisdale Group 33rd User Login</h5>
+        </div>
+   
+    
+<?php
+  LoginFunction();
+?>
+   
+            <div class="card-body">
+              <form action="LoginBackEnd.php" method="POST">
+                <input type="text" name="email_user" placeholder="Email or User Name" value="" class="form-control mb-2">
+                <input type="password" name="password" placeholder="Password" value="" class="form-control mb-3">
+                <table style="width:100%">
+                <tr>
+                <th><button name="login" class="btn btn-outline-secondary">Login</button>
+                <button name="admin" class="btn btn-outline-dark">Login as Admin</button>
+                <!-- <a href="AdminLoginFrontEnd.php" class="btn btn-primary">Administrator Login</a> -->
+                </th>
+                <th><a href="RegisterFrontEnd.php" class="btn btn-sm btn-outline-secondary" style="float:right">Sign up as Register</a></th>
+                </tr>
+                <tr>      
+                <td></td>
+                <td><a href="AdminFrontEnd.php" class="btn btn-sm btn-outline-dark" style="float:right; width:136px;">Sign up as Admin</a></td>
+                </tr>  
+                </table>          
+              </form>
+            </div> 
         </div>
     </div>
-  </div>
-        
-  <div class="row">
-    <div class="col-lg-6">
-      <div class="card"> 
-          <div class="card-body">
-            <form action="LoginBackEnd.php" method="POST">
-              <input type="text" name="email_user" placeholder="Email or User Name" value="" class="form-control mb-2">
-              <input type="password" name="password" placeholder="Password" value="" class="form-control mb-3">
-              <button name="login" class="btn btn-success">Leader Login</button>
-              <a href="AdminLoginFrontEnd.php" class="btn btn-danger">Administrator Login</a>
-              <a href="RegisterFrontEnd.php" class="card-link float-right">Leader Register</a>              
-            </form>
-          </div> 
-      <div>
-    </div>
-  </div>
-
+</div>
+</div>
 </div>
 
-    <?php require_once('includes/footer.php'); ?>
+<?php require_once('includes/footer.php'); ?>
