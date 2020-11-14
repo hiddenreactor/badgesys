8427d57f72068d55ed6b019b29767fdaf69472a3
philbertyu <51773244+philbertyu@users.noreply.github.com>
2020-11-13 17:32:17 -0800

    Update DetailsFetchAll.php

--- DetailsFetchAll.php
+++ DetailsFetchAll.php
@@ -1,6 +1,6 @@
 <?php
 //fetch.php
-$connect = mysqli_connect("localhost", "root", "", "scout");
+$connect = mysqli_connect('us-cdbr-iron-east-01.cleardb.net','b8a2927a50099e','8036e8df','heroku_c1c6c2ef5faa08f');
 $columns = array('MemberID', 'MemberName', 'Color', 'Email', 'Contact');   //this has to match with the db
 
 // $query = "SELECT * FROM inventory ";
