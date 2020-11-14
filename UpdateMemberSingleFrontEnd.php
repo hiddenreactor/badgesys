5aed8c313e07a570b3e877ef9bd9247b51c8806c
philbertyu <51773244+philbertyu@users.noreply.github.com>
2020-11-14 10:00:37 -0800

    Update UpdateMemberSingleFrontEnd.php

--- UpdateMemberSingleFrontEnd.php
+++ UpdateMemberSingleFrontEnd.php
@@ -1,7 +1,7 @@
 
  <?php  
  //fetch.php  
- $connect = mysqli_connect("localhost", "root", "", "scout");  
+ $connect = mysqli_connect("us-cdbr-iron-east-01.cleardb.net", "b8a2927a50099e", "8036e8df", "heroku_c1c6c2ef5faa08f");
  if(isset($_POST["user_id"]))  
  {  
       $query = "SELECT * FROM members WHERE members.MemberID = '".$_POST["user_id"]."'";  
