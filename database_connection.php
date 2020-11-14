c4e6d4ce578a404e5cee3dbe82d8962635b6c413
philbertyu <51773244+philbertyu@users.noreply.github.com>
2020-11-14 09:58:25 -0800

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -1,8 +1,8 @@
 <?php
 
-$username = 'root';
-$password = '';
-$connection = new PDO( 'mysql:host=localhost;dbname=scout', $username, $password );
-// $connection = new PDO("mysql:host=us-cdbr-iron-east-01.cleardb.net; dbname=heroku_c1c6c2ef5faa08f;", "b8a2927a50099e", "8036e8df");
+$username = "b8a2927a50099e";
+$password = "8036e8df";
+// $connection = new PDO( 'mysql:host=localhost;dbname=scout', $username, $password );
+$connection = new PDO("mysql:host=us-cdbr-iron-east-01.cleardb.net; dbname=heroku_c1c6c2ef5faa08f;", $username, $password);
 
 ?>
