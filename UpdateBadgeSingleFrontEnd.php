87a33b3556b33fd5bc532d50e92c5c56e51556ab
philbertyu <51773244+philbertyu@users.noreply.github.com>
2020-11-14 11:43:10 -0800

    connect heroku to clearDB for release 1.0a

--- UpdateBadgeSingleFrontEnd.php
+++ UpdateBadgeSingleFrontEnd.php
@@ -1,7 +1,7 @@
 
  <?php  
  //fetch.php  
- $connect = mysqli_connect("localhost", "root", "", "scout");  
+ $connect = mysqli_connect("us-cdbr-iron-east-01.cleardb.net", "b8a2927a50099e", "8036e8df", "heroku_c1c6c2ef5faa08f");
  if(isset($_POST["badge_id"]))  
  {  
       $query = "SELECT * FROM inventorys, badges, category, badgelevel WHERE 
