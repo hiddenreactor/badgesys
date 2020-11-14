da8105b1ec788a0146435f968f5446a188a138bb
philbertyu <51773244+philbertyu@users.noreply.github.com>
2020-11-14 11:50:30 -0800

    connect heroku to clearDB for release 1.0a

--- get_level.php
+++ get_level.php
@@ -1,7 +1,7 @@
 <?php
 //fetch.php
 
- $connect = mysqli_connect("localhost", "root", "", "scout");
+ $connect = mysqli_connect("us-cdbr-iron-east-01.cleardb.net", "b8a2927a50099e", "8036e8df", "heroku_c1c6c2ef5faa08f");
  $output ='';
 
 $sql = "SELECT * FROM badgelevel WHERE CategoryID='".$_POST["CategoryID"]."' ";
