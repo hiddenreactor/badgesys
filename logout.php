59d925529e1ba62b857f6e74f14ec711564febba
philbertyu <51773244+philbertyu@users.noreply.github.com>
2020-11-13 16:25:06 -0800

    logout.php for release 1.0a

--- logout.php
+++ logout.php
@@ -1,12 +1,12 @@
 <?php require_once('includes/header.php'); ?>
 <?php require_once('includes/function.php'); ?>
-
 <?php
     if(isset($_SESSION['MemberID']) || isset($_SESSION['admin']) || $_POST['logout'])
     {
-        session_start();
+        session_start(); // need to remove
         session_unset();
         session_destroy();
         header("location:index.php");
+        // header( "refresh:5;url=login.php" );
     }
 ?>
