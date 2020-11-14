cceaf1875834371ce1d52f1022dade6e741b692a
philbertyu <51773244+philbertyu@users.noreply.github.com>
2020-11-14 11:39:53 -0800

    connect heroku to clearDB for release 1.0a

--- InventoryDetailsFrontEnd.php
+++ InventoryDetailsFrontEnd.php
@@ -4,7 +4,7 @@
 
 require_once('includes/header.php');
 
-$connect = new PDO("mysql:host=localhost; dbname=scout;", "root", "");
+$connect = new PDO("mysql:host=us-cdbr-iron-east-01.cleardb.net; dbname=heroku_c1c6c2ef5faa08f;", "b8a2927a50099e", "8036e8df");
 $query = "SELECT DISTINCT BadgeName FROM (inventorys INNER JOIN badges ON inventorys.BadgeID = badges.BadgeID)";
 $statement = $connect->prepare($query);
 $statement->execute();
