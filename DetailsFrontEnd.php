6d67132d4efa747cb6891c5f50547a4f4b9d3b92
philbertyu <51773244+philbertyu@users.noreply.github.com>
2020-11-13 17:41:29 -0800

    Update DetailsFrontEnd.php

--- DetailsFrontEnd.php
+++ DetailsFrontEnd.php
@@ -3,7 +3,7 @@
 // require_once('includes/connection.php');
 
 require_once('includes/header.php');
-$connect = new PDO('us-cdbr-iron-east-01.cleardb.net','b8a2927a50099e','8036e8df','heroku_c1c6c2ef5faa08f');
+$connect = new PDO("mysql:host=us-cdbr-iron-east-01.cleardb.net; dbname=heroku_c1c6c2ef5faa08f;", "b8a2927a50099e", "8036e8df");
 
 
 // $query = "SELECT DISTINCT SectionName FROM (members INNER JOIN sections ON members.SectionID = sections.SectionID)";
