aacb61f1b0c77c232c7e5c20696f0e3e7d7131ab
philbertyu <51773244+philbertyu@users.noreply.github.com>
2020-11-13 22:05:00 -0800

    Update AdminPanelFrontEnd.php

--- AdminPanelFrontEnd.php
+++ AdminPanelFrontEnd.php
@@ -3,9 +3,7 @@
 
 require_once('includes/header.php');
 
-
-$connect = new PDO("mysql:host=localhost; dbname=scout;", "root", "");
-
+$connect = new PDO("mysql:host=us-cdbr-iron-east-01.cleardb.net; dbname=heroku_c1c6c2ef5faa08f;", "b8a2927a50099e", "8036e8df");
    
     if (isset($_SESSION['admin'])) {
         $_SESSION['GET'] = $GetID = $_GET['success'];
