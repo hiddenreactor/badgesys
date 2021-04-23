545f3012cbc52c76a778caa01cc2e4373fcb0744
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2021-04-23 12:10:50 -0700

    Update db_connection.php
    update SQL Credential

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -2,7 +2,9 @@
 
 //database_connection.php
 
-$connect = new PDO("mysql:host=us-cdbr-iron-east-01.cleardb.net; dbname=heroku_c1c6c2ef5faa08f;", "b8a2927a50099e", "8036e8df");
+// $connect = new PDO("mysql:host=us-cdbr-iron-east-01.cleardb.net; dbname=heroku_c1c6c2ef5faa08f;", "b8a2927a50099e", "8036e8df");  Old SQL credential
+
+$connect = new PDO("mysql:host=us-cdbr-east-03.cleardb.com; dbname=heroku_d1dabaaefc9d538;", "b9cd122ae5026e", "287b0048");
 
 function fill_select_box($connect, $category_id)
 {
