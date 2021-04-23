1500ce95fbc40d1f372649b61d26e923737e143c
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2021-04-23 12:08:47 -0700

    Update connection.php
    Update SQL connection credential

--- includes/connection.php
+++ includes/connection.php
@@ -1,6 +1,6 @@
 <?php
-
- $con = mysqli_connect('us-cdbr-east-03.cleardb.com','b9cd122ae5026e','287b0048','heroku_d1dabaaefc9d538');
+//update SQL credential
+ $con = mysqli_connect('us-cdbr-east-03.cleardb.com','b9cd122ae5026e','287b0048','heroku_d1dabaaefc9d538'); 
 
   if(!$con)
   {
