a5bbc3fedd27011206c23df0d1fa4d3ab1dbc2a2
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2021-04-24 11:47:44 -0700

    Update AdminPanelFrontEnd.php

--- AdminPanelFrontEnd.php
+++ AdminPanelFrontEnd.php
@@ -3,7 +3,7 @@
 
 require_once('includes/header.php');
 
-$connect = new PDO("mysql:host=us-cdbr-iron-east-01.cleardb.net; dbname=heroku_c1c6c2ef5faa08f;", "b8a2927a50099e", "8036e8df");
+$connect = new PDO("mysql:host=us-cdbr-east-03.cleardb.com; dbname=heroku_d1dabaaefc9d538;", "b9cd122ae5026e", "287b0048");
    
     if (isset($_SESSION['admin'])) {
         $_SESSION['GET'] = $GetID = $_GET['success'];
