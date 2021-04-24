ffafac57e70adaef55f875976fb4dd9f0ed76362
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2021-04-23 17:12:06 -0700

    Add files via upload
    update on function and header page

--- includes/function.php
+++ includes/function.php
@@ -5,42 +5,42 @@
           $Message = "";
           if (isset($_GET['empty'])) {
             $Message = "Please enter all fields";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
             if (isset($_GET['addSuccess'])) {
               $Message = "Badge added successfully";
-              echo '<div class="alert alert-success text-center">'.$Message.'</div>';
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
             }
 
             if (isset($_GET['ValidEmail'])) {
               $Message = "Please enter valid Email";
-              echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
             }
 
             if (isset($_GET['UserTaken'])) {
               $Message = "User name already taken";
-              echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
             }
 
             if (isset($_GET['date'])) {
               $Message = "Incorrect date format";
-              echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
             }
 
             if (isset($_GET['EmailTaken'])) {
               $Message = "Email already taken";
-              echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
             }
 
             if (isset($_GET['InvalidFormat'])) {
               $Message = "Invalid image format";
-              echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
             }
 
             if (isset($_GET['UnsuccessInsert'])) {
               $Message = "Data Insert Unsuccessfully";
-              echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
             }
 
             if (isset($_GET['success'])) {
@@ -58,47 +58,47 @@
 
           if (isset($_GET['empty'])) {
             $Message = "Please enter all fields";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
           if (isset($_GET['code'])) {
             $Message = "Please enter valid access code";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
           if (isset($_GET['character'])) {
             $Message = "Please enter valid character";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
           if (isset($_GET['ValidEmail'])) {
             $Message = "Please enter valid Email";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
           if (isset($_GET['UserTaken'])) {
             $Message = "User name already taken";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
           if (isset($_GET['phone'])) {
             $Message = "Incorrect phone format";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
           if (isset($_GET['EmailTaken'])) {
             $Message = "Email already taken";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
           if (isset($_GET['InvalidFormat'])) {
             $Message = "Invalid image format";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
           if (isset($_GET['InvalidSize'])) {
             $Message = "Image size too large";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
           if (isset($_GET['success'])) {
@@ -115,7 +115,7 @@
           $Message = "";
           if (isset($_GET['empty'])) {
             $Message = "Please enter all fields";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
 
@@ -138,24 +138,94 @@
             if(isset($_GET['empty']))
             {
               $Message = " Please fill in the blanks ";
-              echo ' <div class="alert alert-danger text-center">'.$Message.'</div> ';
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
             }
 
             if(isset($_GET['invalid']))
             {
               $Message = " User name Not Match ";
-              echo ' <div class="alert alert-danger text-center">'.$Message.'</div> ';
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
             }
 
             if(isset($_GET['pass_invalid']))
             {
               $Message = " Password Incorrect ";
-              echo ' <div class="alert alert-danger text-center">'.$Message.'</div> ';
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
+            }
+
+            if(isset($_GET['status']))
+            {
+              $Message = " User not exist ";
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
             }
 
             if(isset($_SESSION['MemberID']))
             {
               header("location:index.php");
+            }
+        }
+
+        function AdminLoginFunction() {
+          $Message = "";
+            if(isset($_GET['empty']))
+            {
+              $Message = " Please fill in the blanks ";
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
+            }
+
+            if(isset($_GET['invalid']))
+            {
+              $Message = " User name Not Match ";
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
+            }
+
+            if(isset($_GET['leader_password_invalid']))
+            {
+              $Message = " Password Incorrect ";
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
+            }
+
+            if(isset($_GET['status']))
+            {
+              $Message = " User not exist ";
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
+            }
+
+            if(isset($_SESSION['MemberID']))
+            {
+              header("location:index.php");
+            }
+        }
+
+        function MemberLoginFunction() {
+          $Message = "";
+            if(isset($_GET['empty']))
+            {
+              $Message = " Please fill in the blanks ";
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
+            }
+
+            if(isset($_GET['invalid']))
+            {
+              $Message = " User name Not Match ";
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
+            }
+
+            if(isset($_GET['member_password_invalid']))
+            {
+              $Message = " Password Incorrect ";
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
+            }
+
+            if(isset($_GET['member_status']))
+            {
+              $Message = " User not exist ";
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
+            }
+
+            if(isset($_SESSION['memberlogin']))
+            {
+              header("location:memberlogin.php");
             }
         }
 
@@ -165,37 +235,43 @@
 
           if (isset($_GET['empty'])) {
             $Message = "Please enter all fields";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
           if (isset($_GET['character'])) {
             $Message = "Please enter First Name and Last Name seprate with space";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
           if (isset($_GET['ValidEmail'])) {
             $Message = "Please enter valid Email";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
           if (isset($_GET['phone'])) {
             $Message = "Pleae enter valid phone number";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
           if (isset($_GET['MemberTaken'])) {
             $Message = "Member already exist";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
           if (isset($_GET['EmailTaken'])) {
             $Message = "Email already taken";
-            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
+
+          if(isset($_GET['status']))
+            {
+              $Message = " User not exist ";
+              echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
+            }
 
           if (isset($_GET['success'])) {
             $Message = "You have successfully added a member";
-            echo '<div class="alert alert-success text-center">'.$Message.'</div>';
+            echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
           }
 
       }
@@ -205,19 +281,19 @@
 
         if(isset($_GET['empty'])) {
           $Message = " Enter Valid User Name and password ";
-          echo ' <div class="alert alert-danger text-center">'.$Message.'</div> ';
+          echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
         }
 
         if(isset($_GET['invalid']))
         {
           $Message = " User name Not Match ";
-          echo ' <div class="alert alert-danger text-center">'.$Message.'</div> ';
+          echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
         }
 
         if(isset($_GET['passord_invalid']))
         {
           $Message = " Password Incorrect ";
-          echo ' <div class="alert alert-danger text-center">'.$Message.'</div> ';
+          echo ' <div class="alert text-center" style="background:#D7D7D7; color:#7B7B7B">'.$Message.'</div> ';
         }
 
         if(isset($_SESSION['AdminID']))
--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,7 @@ ob_start();
 <script src="https://rawgit.com/RobinHerbots/jquery.inputmask/3.x/dist/jquery.inputmask.bundle.js"></script>
 
 <script src="http://parsleyjs.org/dist/parsley.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.9.1/parsley.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.9.2/parsley.min.js"></script>
  
 <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
@@ -23,12 +23,11 @@ ob_start();
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.68/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.print.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.4/js/pdfmake.min.js"></script>
 
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css"> 
@@ -49,27 +48,57 @@ ob_start();
   {
    margin:0;
    padding:0;
-   background-color:#f1f1f1;
+   /*  Web background--beige */
+   background: #EEEEEE;
   }
-  .box
-  {
-   width:1200px;
-   padding:20px;
-   background-color:#fff;
-   border:1px solid #ccc;
-   border-radius:5px;
-   margin-top:25px;
-   box-sizing:border-box;
-  }
-  table.dataTable thead .none.sorting:after {
-    display: none;
-    input.parsley-success,
- select.parsley-success,
- textarea.parsley-success {
-   color: #468847;
-   background-color: #DFF0D8;
-   border: 1px solid #D6E9C6;
- }
+/* add background-color to active links--Navy Blue */
+.navbar-custom {
+   /* Navbar */
+    background-color: #293E6A;
+}
+
+/* change the brand and text color--Gold #B6A754, now silver #C0C0C0 */
+.navbar-custom .navbar-brand,
+.navbar-nav .nav-link,
+.nav-item .btn {
+    color: #C0C0C0;  
+}
+/* Card title--Navy Blue */
+.card-title.custom {
+  background: #293E6A;
+}
+/* Card title text--Gold */
+.card-title .text-center.custom,
+.card-body {
+  color: #C0C0C0;
+}
+/* Card body--Navy Blue*/
+.card-body.custom {
+  background: #293E6A;
+}
+/* Card body strip--Gold */
+.card.custom {
+  background: #C0C0C0;
+}
+.box
+{
+  width:1200px;
+  padding:20px;
+  background-color:#fff;
+  border:1px solid #ccc;
+  border-radius:5px;
+  margin-top:25px;
+  box-sizing:border-box;
+}
+table.dataTable thead .none.sorting:after {
+  display: none;
+  input.parsley-success,
+select.parsley-success,
+textarea.parsley-success {
+  color: #468847;
+  background-color: #DFF0D8;
+  border: 1px solid #D6E9C6;
+}
 
  input.parsley-error,
  select.parsley-error,
@@ -146,7 +175,7 @@ ob_start();
 }
 </style>
   <body>
-    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+    <nav class="navbar navbar-expand-sm navbar-custom">
       <div class="container">
         <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar" type="button" name="button">
           <span class="navbar-toggler-icon"></span>
@@ -156,12 +185,12 @@ ob_start();
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
             <?php
-                if (isset($_SESSION['MemberID']) || isset($_SESSION['admin'])) {
+                if (isset($_SESSION['MemberID']) || isset($_SESSION['admin']) || isset($_SESSION['memberlogin'])) {
                     echo '<form action="logout.php" method="POST">
                         <button class=btn btn-link name="logout">Logout</button>
                         </form>';
                 } else {
-                    echo '<a href="index.php" class="nav-link">Login</a>';
+                    echo '<a href="/badgesys/index.php" class="nav-link">Login</a>';
                 }
               ?>
             </li>
