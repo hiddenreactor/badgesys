708ba89b9b4dd497d0dd1c666fb5f522e5aa2432
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2021-04-24 12:41:25 -0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php require_once('includes/header.php'); ?>
 <?php require_once('includes/function.php'); ?>
 <?php
-    if(isset($_SESSION['MemberID']) || isset($_SESSION['admin']) || $_POST['logout'])
+    if(isset($_SESSION['MemberID']) || isset($_SESSION['admin']) || isset($_SESSION['memberlogin']) || $_POST['logout'])
     {
         session_start(); // need to remove
         session_unset();
