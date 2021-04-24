0e9fbd52af0f98a264940a836bc33482e6e5290a
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2021-04-23 17:34:08 -0700

    Update connection.php

--- includes/config/connection.php
+++ includes/config/connection.php
@@ -31,5 +31,5 @@ $pdoOptions = array(
 //     MYSQL_PASSWORD, //Password
 //     $pdoOptions //Options
 // );
-$connect = new PDO("mysql:host=us-cdbr-iron-east-01.cleardb.net; dbname=heroku_c1c6c2ef5faa08f;", "b8a2927a50099e", "8036e8df", $pdoOptions);
+$connect = new PDO("mysql:host=us-cdbr-east-03.cleardb.com; dbname=heroku_d1dabaaefc9d538;", "b9cd122ae5026e", "287b0048", $pdoOptions);
 ?>
