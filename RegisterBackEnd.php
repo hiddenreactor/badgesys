9c0eb8a9902ff4914aa9ac0ca7892309bf9ddcef
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2021-04-29 12:52:30 -0700

    Update RegisterBackEnd.php
    http redirect upon success registeration

--- RegisterBackEnd.php
+++ RegisterBackEnd.php
@@ -1,12 +1,37 @@
 <?php
+require_once('lib/access.php');
+require_once('includes/config/connection.php');
 ob_start();
-include_once('lib/access.php');
-include_once('includes/config/connection.php');
 // sleep(1);
+
+if(isset($_POST['FName']))
+if ($AccessCode !== $_POST['Access']) {
+echo 'Invalid Access Code!';
+// echo '<meta http-equiv="refresh" content="2;url=index.php">';
+return;
+}
+else {
+ 
+ $data = array(
+  ':fname'  => $_POST['FName'],
+  ':lname'  => $_POST['LName'],
+  ':username'  => $_POST['UName'],
+  ':email'   => $_POST['Email'],
+  ':password'   => password_hash($_POST['Password'], PASSWORD_DEFAULT),
+  ':date' =>  date("d/m/y")
+ );
+  
+ $query = " INSERT INTO user_data (FName, LName, UName, Email, Password, Date) VALUES (:fname, :lname, :username, :email, :password, :date)";
+ 
+ $statement = $connect->prepare($query);
+ if ($statement->execute($data)) {
+    echo 'Registration Completed Successfully...'; 
+    exit();
+ }
+}
 
 if(isset($_POST["email"]))
 {
-
 
  $query = " SELECT * FROM user_data WHERE Email = '".$_POST["email"]."'";
 
@@ -29,7 +54,6 @@ if(isset($_POST["email"]))
 if(isset($_POST["username"]))
 {
 
-
  $query = " SELECT * FROM user_data WHERE UName = '".$_POST["username"]."' ";
 
  $statement = $connect->prepare($query);
@@ -47,37 +71,4 @@ if(isset($_POST["username"]))
   echo json_encode($output);
  }
 }
-
-
-if(isset($_POST['FName']))
-if ($AccessCode !== $_POST['Access']) {
-echo 'Invalid Access Code!';
-echo '<meta http-equiv="refresh" content="2;url=index.php">';
-return;
-}
-else {
-
-    {
-
- 
- $data = array(
-  ':fname'  => $_POST['FName'],
-  ':lname'  => $_POST['LName'],
-  ':username'  => $_POST['UName'],
-  ':email'   => $_POST['Email'],
-  ':password'   => password_hash($_POST['Password'], PASSWORD_DEFAULT),
-  ':date' =>  date("d/m/y")
- );
-  
- $query = " INSERT INTO user_data (FName, LName, UName, Email, Password, Date) VALUES (:fname, :lname, :username, :email, :password, :date)";
- $statement = $connect->prepare($query);
- if ($statement->execute($data)) {
-//      echo 'Registration Completed Successfully...';
-        header( "refresh:1;url=index.php" );
-//      session_unset();
-    //  header( "refresh:2;url=index.php" );
- }
-}
-}
-ob_end_flush();
 ?>
