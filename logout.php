7ca3d53650291d343ee44fcd31555936bb937ae4
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2021-05-02 13:12:15 -0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,12 +1,29 @@
 <?php require_once('includes/header.php'); ?>
 <?php require_once('includes/function.php'); ?>
 <?php
-    if(isset($_SESSION['MemberID']) || isset($_SESSION['admin']) || isset($_SESSION['memberlogin']) || $_POST['logout'])
+    if(isset($_SESSION['MemberID']) || ($_POST['logout']))
     {
         session_start(); // need to remove
         session_unset();
         session_destroy();
         header("location:index.php");
-        // header( "refresh:5;url=login.php" );
+        exit();          
+    }
+    elseif(isset($_SESSION['admin']) || ($_POST['logout']))
+    {
+        session_start(); // need to remove
+        session_unset();
+        session_destroy();
+        header("location:admin.php"); 
+        exit();         
+    }
+    elseif(isset($_SESSION['memberlogin']) || ($_POST['logout']))
+    {
+        session_start(); // need to remove
+        session_unset();
+        session_destroy();
+        header("location:memberlogin.php"); 
+        exit();           
     }
 ?>
+
